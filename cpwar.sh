0f381a4e052d90ba0fb8d62b17ddcd39b8ab009e
rsearls <rsearls@redhat.com>
2015-07-16 11:26:19 -0400

    initial set of wars and scripts

---
+++ cpwar.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -x
+
+cp $1  /home/rsearls/j1/wise-module/myWildfly/wildfly/dist/target/wildfly-9.0.0.CR2/standalone/deployments/.
+##cp target/wise-gwt-gui-1.0-SNAPSHOT.war  /home/rsearls/j1/wise-module/myWildfly/wildfly/dist/target/wildfly-9.0.0.CR2/standalone/deployments/.
---
+++ rmwar.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -x
+
+rm /home/rsearls/j1/wise-module/myWildfly/wildfly/dist/target/wildfly-9.0.0.CR2/standalone/deployments/$1
+##rm /home/rsearls/j1/wise-module/myWildfly/wildfly/dist/target/wildfly-9.0.0.CR2/standalone/deployments/wise-gwt-gui-1.0-SNAPSHOT.war
